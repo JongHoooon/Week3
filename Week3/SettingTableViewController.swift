d7ac79af5d8ac4815b3d52f60dc9dafd87fe7518
JongHoon <qnm541@gmail.com>
2023-08-01 11:32:55 +0900

    refactor setting

--- Week3/SettingTableViewController.swift
+++ Week3/SettingTableViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+enum SettingOptions: Int, CaseIterable {
+    case total
+    case personal
+    case others
+    
+    var mainOpitons: String {
+        switch self {
+        case .total:        return "전체 설정"
+        case .personal:     return "개인 설정"
+        case .others:       return "기타"
+        }
+    }
+    
+    var subOptions: [String] {
+        switch self {
+        case .total:        return ["공지사항", "실험실", "버전 정보"]
+        case .personal:     return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:       return ["고객센터/도움말"]
+        }
+    }
+}
+
 final class SettingTableViewController: UITableViewController {
     
     // MARK: - Properties
@@ -32,7 +54,8 @@ extension SettingTableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return settings.count
+//        return settings.count
+        SettingOptions.allCases.count
     }
 
     override func tableView(
@@ -40,7 +63,8 @@ extension SettingTableViewController {
         numberOfRowsInSection section: Int
     ) -> Int {
         
-        return settings[section].count
+//        return settings[section].count
+        return SettingOptions.allCases[section].subOptions.count
     }
 
     override func tableView(
@@ -50,7 +74,11 @@ extension SettingTableViewController {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell") else { return UITableViewCell() }
         
-        cell.textLabel?.text = settings[indexPath.section][indexPath.row]
+//        cell.textLabel?.text = settings[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = SettingOptions
+            .allCases[indexPath.section]
+            .subOptions[indexPath.row]
         
         return cell
     }
@@ -60,6 +88,9 @@ extension SettingTableViewController {
         titleForHeaderInSection section: Int
     ) -> String? {
         
-        return headerTitiles[section]
+//        return headerTitiles[section]
+        return SettingOptions
+            .allCases[section]
+            .mainOpitons
     }
 }
